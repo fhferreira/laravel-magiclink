26eb44496c4d1f82a9a88418ec1168690f660bdd
Flávio H. Ferreira <flaviometalvale@gmail.com>
2018-12-19 02:40:34 -0200

    Update MagicLink.php

--- src/Models/MagicLink.php
+++ src/Models/MagicLink.php
@@ -3,9 +3,12 @@
 namespace Cesargb\MagicLink\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MagicLink extends Model
 {
+    use SoftDeletes;
+
     public function user()
     {
         return $this->hasOne(config('auth.providers.users.model'), config('magiclink.user_primarykey', 'id'), 'users_id');
